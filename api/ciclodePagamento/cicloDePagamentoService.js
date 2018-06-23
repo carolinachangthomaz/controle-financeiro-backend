1beee9b5d1a83e615742f0576bf21f06ae98e604
Carolina Thomaz <carol.com.jp@gmail.com>
2018-06-23 14:28:51 -0300

    Serviço - Contador

--- api/ciclodePagamento/cicloDePagamentoService.js
+++ api/ciclodePagamento/cicloDePagamentoService.js
@@ -2,4 +2,15 @@ const CicloDePagamento = require('./cicloDePagamento')
 
 CicloDePagamento.methods(['get', 'post', 'put', 'delete'])
 CicloDePagamento.updateOptions({new:true, runValidators: true})
+
+CicloDePagamento.route('contador', function(req, res, next){
+    CicloDePagamento.count(function(error,value){
+         if(error){
+             res.status(500).json({errors:[error]})
+         }else{
+             res.json({value})
+         }
+    })
+})
+
 module.exports = CicloDePagamento
