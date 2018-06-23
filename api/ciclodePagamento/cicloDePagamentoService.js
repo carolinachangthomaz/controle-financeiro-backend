17b2cb7ab223ddd5f1ac37730ee70c153f726952
Carolina Thomaz <carol.com.jp@gmail.com>
2018-06-23 16:20:15 -0300

    Padronizando mensagens de erro

--- api/ciclodePagamento/cicloDePagamentoService.js
+++ api/ciclodePagamento/cicloDePagamentoService.js
@@ -1,7 +1,26 @@
+const _ = require('lodash')
 const CicloDePagamento = require('./cicloDePagamento')
 
 CicloDePagamento.methods(['get', 'post', 'put', 'delete'])
 CicloDePagamento.updateOptions({new:true, runValidators: true})
+CicloDePagamento.after('post', sendErrorsOrNext).after('put',sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next){
+    const bundle = res.locals.bundle
+
+    if(bundle.errors){
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    }else{
+        next()
+    }
+}
+
+function parseErrors(nodeRestFulErrors){
+    const errors = []
+    _.forIn(nodeRestFulErrors, error => errors.push(error.message))
+    return errors
+}
 
 CicloDePagamento.route('contador', function(req, res, next){
     CicloDePagamento.count(function(error,value){
