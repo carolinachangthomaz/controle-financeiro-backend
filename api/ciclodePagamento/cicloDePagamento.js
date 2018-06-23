a7f70972d0bf42232b7994778975369fc6212d6d
Carolina Thomaz <carol.com.jp@gmail.com>
2018-06-23 12:44:58 -0300

    Schema - Ciclo de Pagamento

---
+++ api/ciclodePagamento/cicloDePagamento.js
@@ -0,0 +1,26 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+
+const creditoSchema = new mongoose.Schema({
+    nome:{type:String,required: true},
+    valor: {type: Number, min: 0, required: true} 
+})
+
+const debitoSchema = new mongoose.Schema({
+    nome:{type:String,required: true},
+    valor: {type: Number, min: 0, required: true},
+    status: {type: string, required: false, uppercase: true,
+      enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+})
+
+const ciclodePagamentoSchema = new mongoose.Schema({
+    nome:{type:String,required: true},
+    mes: {type: Number, min: 1, max: 12, required: true},
+    ano: {type: Number, min: 1970, max: 2100, required: true},
+    creditos: [creditoSchema],
+    debitos: [debitoSchema],
+})
+
+
+module.exports = restful.model('CicloDePagamento', ciclodePagamentoSchema)
